c43bb229f16e71e6a2ee9d599917285a3f29737e
Lawrenciumbc <lawrenciumbc@hotmail.com>
2020-05-28 00:03:29 +0100

    debug again

--- app.py
+++ app.py
@@ -13,5 +13,6 @@ def takeStill():
     camCommands.photo()
     return render_template('index.html')
 
-#if __name__ == '__main__':
-#    app.run(debug=True, host='0.0.0.0')
+# make externally visible
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
