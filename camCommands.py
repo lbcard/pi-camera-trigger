a27f97bd26dd7e9cd56374a1bdf2948f6385bceb
Lawrenciumbc <lawrenciumbc@hotmail.com>
2020-05-28 02:09:45 +0100

    fixed cm dir

--- camCommands.py
+++ camCommands.py
@@ -1,12 +1,12 @@
 import subprocess
 import time
 
-stillsDir = "~/Desktop/cameraShots/"
 vidsDir = "~/Desktop/cameraVids/"
 
 def photo():
+    stillsDir = "~/Desktop/cameraShots/"
     epoch_time = int(time.time())
-    takeStill = subprocess.run(["raspistill", "-o stillsDir%d.jpg" % epoch_time])
+    takeStill = subprocess.run(["raspistill", "-o %s%d.jpg" % (stillsDir, epoch_time)])
     print ("takeStill response: ")
     print (takeStill)
     return "Photo Triggered %d.jpg" % epoch_time
