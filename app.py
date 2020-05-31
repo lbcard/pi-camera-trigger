e78352d7fce5b7269b4cb39c9337a56ebd5d9fa2
Lawrenciumbc <lawrenciumbc@hotmail.com>
2020-05-31 15:31:04 +0100

    changed image list generation

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 from flask import Flask, render_template
 import camCommands
+from os import listdir
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html', response="", message="")
+    imageList = os.listdir('./static/images/')
+    return render_template('index.html', response="", message="" images=imageList)
+
 
 @app.route('/takeStill')
 def takeStill():
-    print ("take still triggered")
+    print("take still triggered")
     photoResult = camCommands.photo()
-    return render_template('index.html', response="Photo Taken", message=photoResult)
+    imageList = os.listdir('./static/images/')
+    return render_template('index.html', response="Photo Taken", message=photoResult images=imageList)
+
 
 # make externally visible
 # doesn't appear to do anything have to launch as flask run --host=0.0.0.0
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
